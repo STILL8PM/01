f8af994e260b50da155ae391b8e9cb9cf03f47c9
pw <1040479402@qq.com>
2021-04-16 17:08:58 +0800

    看完了

---
+++ main copy 2.js
@@ -0,0 +1,50 @@
+/*console.log(window);//浏览器打印window对象
+window.alert(1);//不需要写window,一切皆window对象
+alert(1);*/
+
+
+
+//Single element 单元素选择器
+//console.log (document.getElementById('my-form'));
+/*const form = document.getElementById('my-form');//存储到form
+console.log(form);//打印到控制台*/
+/*console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+console.log(document.querySelector('h1'));*/
+/*getXXXByXXX 获取的是动态集合，querySelector获取的是静态集合。
+简单的说就是，动态就是选出的元素会随文档改变，静态的不会，
+取出来之后就和文档的改变无关了。*/
+
+
+// Multiple element 多元素选择器
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+/* const items = document.querySelectorAll('.item');//新建变量items
+items.forEach((item) => console.log(item));//遍历items中的item，并打印
+ */
+
+// const ul = document.querySelector('.items');
+// ul.remove();//删除全部ul
+// ul.lastElementChild.remove();//删除最后一个
+ /* ul.firstElementChild.textContent = 'Hello';
+ ul.children[1].innerText = 'Brad';
+ ul.lastElementChild.innerHTML = '<h1>hello</h1>';
+
+ const btn = document.querySelector('.btn');
+ btn.style.background = 'red'; */
+
+/*  const btn = document.querySelector ('.btn');
+ btn.addEventListener('mouseout', (e) => {
+     e.preventDefault();
+     document.querySelector('#my-form').style.background = '#ccc';
+     document.querySelector('body').classList.add('bg-dark');
+     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+ });
+ */
+
+
+
+
+
--- main.js
+++ main.js
@@ -1,38 +1,23 @@
-/*console.log(window);//浏览器打印window对象
-window.alert(1);//不需要写window,一切皆window对象
-alert(1);*/
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
+myForm.addEventListener('submit', onSubmit);
 
+function onSubmit(e) {
+    e.preventDefault();
 
-//Single element 单元素选择器
-//console.log (document.getElementById('my-form'));
-/*const form = document.getElementById('my-form');//存储到form
-console.log(form);//打印到控制台*/
-/*console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-console.log(document.querySelector('h1'));*/
-/*getXXXByXXX 获取的是动态集合，querySelector获取的是静态集合。
-简单的说就是，动态就是选出的元素会随文档改变，静态的不会，
-取出来之后就和文档的改变无关了。*/
-
-
-// Multiple element 多元素选择器
-// console.log(document.querySelectorAll('.item'));
-// console.log(document.getElementsByClassName('item'));
-// console.log(document.getElementsByTagName('li'));
-
-/* const items = document.querySelectorAll('.item');//新建变量items
-items.forEach((item) => console.log(item));//遍历items中的item，并打印
- */
-
-const ul = document.querySelector('.items');
-// ul.remove();//删除全部ul
-// ul.lastElementChild.remove();//删除最后一个
- 
-
-
-
-
-
-
-
+    if(nameInput.value === ''|| emailInput.value ===''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    }else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));    
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+} 
